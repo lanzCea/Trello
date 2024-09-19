7ae809117f8ac514bc674e81edcc8b75300cf930
Cesar Alvarado <caalvaradosys@gmail.com>
2024-09-18 21:30:26 -0500

    Update task

--- TrelloRealm/src/app/[id].tsx
+++ TrelloRealm/src/app/[id].tsx
@@ -1,12 +1,27 @@
 import { Stack, useLocalSearchParams } from "expo-router";
-import { Text, View } from "react-native";
-import { useObject } from "@realm/react";
+import { Text, View, TextInput } from "react-native";
+import { useObject, useRealm } from "@realm/react";
 import { Task } from "../models/Task";
 import { BSON } from "realm";
+import React, { useState } from "react";
+import { update } from "lodash";
 
 const TaskDetails = () => {
   const { id } = useLocalSearchParams();
   const task = useObject<Task>(Task, new BSON.ObjectID(id as string));
+  const [updatedDescription, setUpdateDescription] = useState(
+    task?.description
+  );
+  const realm = useRealm();
+
+  const updateDescription = () => {
+    if (!task) {
+      return;
+    }
+    realm.write(() => {
+      task.description = updatedDescription;
+    });
+  };
 
   if (!task) {
     return <Text>Not foud</Text>;
@@ -15,7 +30,12 @@ const TaskDetails = () => {
   return (
     <View style={{ padding: 10 }}>
       <Stack.Screen options={{ title: "Task details" }} />
-      <Text style={{ color: "white", fontSize: 20 }}>{task.description}</Text>
+      <TextInput
+        value={updatedDescription}
+        onChangeText={setUpdateDescription}
+        onEndEditing={updateDescription}
+        style={{ color: "white", fontSize: 20 }}
+      />
     </View>
   );
 };
