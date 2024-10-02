5118f5fd33a4bf35374bc6fbdd4c2e1b20f851ba
Cesar Alvarado <caalvaradosys@gmail.com>
2024-10-02 18:23:01 -0500

    darg initial position

--- TrelloRealm/src/components/TaskBoard.tsx
+++ TrelloRealm/src/components/TaskBoard.tsx
@@ -1,16 +1,20 @@
-import { View, StyleSheet, SafeAreaView } from 'react-native';
-import TaskList from './TaskList';
-import { LinearGradient } from 'expo-linear-gradient';
-import TaskDragArea from './TaskDragArea';
+import { View, StyleSheet, SafeAreaView } from "react-native";
+import TaskList from "./TaskList";
+import { LinearGradient } from "expo-linear-gradient";
+import TaskDragArea from "./TaskDragArea";
 
 export default function TaskBoard() {
   return (
-        colors={['#8711c1', '#2472fc']}
-        style={StyleSheet.absoluteFill}
-      />
-      <TaskDragArea>
+    <TaskDragArea>
+      <View style={{ padding: 10, flex: 1 }}>
+        <LinearGradient
+          // Background Linear Gradient
+          colors={["#8711c1", "#2472fc"]}
+          style={StyleSheet.absoluteFill}
+        />
+
         <TaskList />
-      </TaskDragArea>
-    </View>
+      </View>
+    </TaskDragArea>
   );
 }
--- TrelloRealm/src/components/TaskDragArea.tsx
+++ TrelloRealm/src/components/TaskDragArea.tsx
@@ -1,13 +1,32 @@
-import { PropsWithChildren, createContext, useContext, useState } from 'react';
+import { View, StyleSheet, useWindowDimensions } from "react-native";
+import DraggingTask from "./DraggingTask";
+import { BSON } from "realm";
+
+import { Gesture, GestureDetector } from "react-native-gesture-handler";
+import Animated, { runOnJS, useSharedValue } from "react-native-reanimated";
+import { PropsWithChildren, createContext, useContext, useState } from "react";
 
 type DraggingContext = {
   setDraggingTask: (id: BSON.ObjectID, y: number) => void;
 };
 
 const DraggingContext = createContext<DraggingContext>({
+  setDraggingTask: () => {},
+});
+
+const TaskDragArea = ({ children }: PropsWithChildren) => {
+  const [draggingTaskId, setDraggingTaskId] = useState<BSON.ObjectID>(null);
+
+  const { width } = useWindowDimensions();
+
+  const dragX = useSharedValue(0);
   const dragY = useSharedValue(0);
 
   const pan = Gesture.Pan()
+    .manualActivation(true)
+    .onTouchesMove((event, stateManager) => {
+      stateManager.activate();
+    })
     .onChange((event) => {
       dragX.value = dragX.value + event.changeX;
       dragY.value = dragY.value + event.changeY;
@@ -18,40 +37,36 @@ const DraggingContext = createContext<DraggingContext>({
 
   const setDraggingTask = (id: BSON.ObjectID, y: number) => {
     setDraggingTaskId(id);
+
     dragY.value = y;
     dragX.value = 20;
   };
 
   return (
     <DraggingContext.Provider value={{ setDraggingTask }}>
-      {children}
       <GestureDetector gesture={pan}>
-        {draggingTaskId ? (
-          <View
+        <View
+          style={{
+            ...StyleSheet.absoluteFill,
+          }}
+        >
+          {children}
+          <Animated.View
             style={{
-              ...StyleSheet.absoluteFill,
-              backgroundColor: 'rgba(100,100,100, 0.8)',
+              width: width - 40,
+              position: "absolute",
+              top: dragY,
+              left: dragX,
+              transform: [
+                {
+                  rotateZ: "3deg",
+                },
+              ],
             }}
           >
-            <Animated.View
-              style={{
-                width: width - 40,
-                position: 'absolute',
-                top: dragY,
-                left: dragX,
-                transform: [
-                  {
-                    rotateZ: '3deg',
-                  },
-                ],
-              }}
-            >
-              <DraggingTask id={draggingTaskId} />
-            </Animated.View>
-          </View>
-        ) : (
-          <View />
-        )}
+            {draggingTaskId && <DraggingTask id={draggingTaskId} />}
+          </Animated.View>
+        </View>
       </GestureDetector>
     </DraggingContext.Provider>
   );
