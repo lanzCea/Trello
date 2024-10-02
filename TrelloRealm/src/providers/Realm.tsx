146a74e11b2f898341150fba9f09b16d5f4be49f
Cesar Alvarado <caalvaradosys@gmail.com>
2024-10-02 17:47:05 -0500

    Setup cloud sync with Atlas App Service

--- TrelloRealm/src/providers/Realm.tsx
+++ TrelloRealm/src/providers/Realm.tsx
@@ -1,17 +1,29 @@
 import { PropsWithChildren } from "react";
-import Realm from "realm";
 import { RealmProvider, AppProvider, UserProvider } from "@realm/react";
 import { Task } from "../models/Task";
-import { Text } from "react-native";
 import Login from "../components/Login";
-const appId = "trello-mjcyr";
 
-export default function RealmCustomProvider({ children }: PropsWithChildren) {
-  return (
-    <AppProvider id={appId}>
-      <UserProvider fallback={Login}>
-        <RealmProvider schema={[Task]}>{children}</RealmProvider>
-      </UserProvider>
-    </AppProvider>
-  );
-}
+const appId = "trello-mjcyr";
+return (
+  <AppProvider id={appId}>
+    <UserProvider fallback={Login}>
+      <RealmProvider
+        schema={[Task]}
+        sync={{
+          flexible: true,
+          onError: (_session, error) => {
+            console.log(error);
+          },
+          initialSubscriptions: {
+            update(subs, realm) {
+              subs.add(realm.objects("Task"));
+            },
+            rerunOnOpen: true,
+          },
+        }}
+      >
+        {children}
+      </RealmProvider>
+    </UserProvider>
+  </AppProvider>
+);
