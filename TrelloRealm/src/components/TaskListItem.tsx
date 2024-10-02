3b5404bc64e9672f18ad04bfaf89cbf3458a6c82
Cesar Alvarado <caalvaradosys@gmail.com>
2024-10-02 18:02:29 -0500

    position field and sorting

--- TrelloRealm/src/components/TaskListItem.tsx
+++ TrelloRealm/src/components/TaskListItem.tsx
@@ -2,22 +2,23 @@ import { View, Text, StyleSheet, Pressable } from "react-native";
 import { AntDesign } from "@expo/vector-icons";
 import { Link } from "expo-router";
 import { Task } from "../models/Task";
-import { useReducer } from "react";
 import { useRealm } from "@realm/react";
 
 export default function TaskListItem({ task }: { task: Task }) {
-  const real = useRealm();
+  const realm = useRealm();
 
   const deleteTask = () => {
-    real.write(() => {
-      real.delete(task);
+    realm.write(() => {
+      realm.delete(task);
     });
   };
 
   return (
     <Link href={`/${task._id}`} asChild>
       <Pressable style={styles.container}>
-        <Text style={styles.text}>{task.description}</Text>
+        <Text style={styles.text}>
+          {task.position}: {task.description}
+        </Text>
 
         <AntDesign onPress={deleteTask} name="close" size={16} color="gray" />
       </Pressable>
@@ -28,10 +29,13 @@ export default function TaskListItem({ task }: { task: Task }) {
 const styles = StyleSheet.create({
   container: {
     backgroundColor: "#1D2125",
-    padding: 10,
+    padding: 15,
     borderRadius: 5,
     flexDirection: "row",
     justifyContent: "space-between",
   },
-  text: { color: "white", fontSize: 16 },
+  text: {
+    color: "white",
+    fontSize: 16,
+  },
 });
--- TrelloRealm/src/models/Task.ts
+++ TrelloRealm/src/models/Task.ts
@@ -1,12 +1,13 @@
-import { property } from "lodash";
-import { BSON, Realm } from "realm";
+import { BSON, Realm } from 'realm';
 
+// Define your object model
 export class Task extends Realm.Object<Task> {
   _id: BSON.ObjectId = new BSON.ObjectId();
   description!: string;
   isComplete: boolean = false;
-  createAt: Date = new Date();
+  createdAt: Date = new Date();
+
+  position: Realm.Types.Int = 0;
   user_id!: string;
 
-  static primaryKey = "_id";
-}
+  static primaryKey = '_id'
