19ff060942192a084ad91b8d70dcb827dc9ae564
Cesar Alvarado <caalvaradosys@gmail.com>
2024-10-02 17:58:32 -0500

    Profile page

--- TrelloRealm/src/providers/Realm.tsx
+++ TrelloRealm/src/providers/Realm.tsx
@@ -1,29 +1,32 @@
 import { PropsWithChildren } from "react";
 import { RealmProvider, AppProvider, UserProvider } from "@realm/react";
 import { Task } from "../models/Task";
-import Login from "../components/Login";
+import AnonymousLogin from "../components/AnonymousLogin";
 
 const appId = "trello-mjcyr";
-return (
-  <AppProvider id={appId}>
-    <UserProvider fallback={Login}>
-      <RealmProvider
-        schema={[Task]}
-        sync={{
-          flexible: true,
-          onError: (_session, error) => {
-            console.log(error);
-          },
-          initialSubscriptions: {
-            update(subs, realm) {
-              subs.add(realm.objects("Task"));
+
+export default function RealmCustomProvider({ children }: PropsWithChildren) {
+  return (
+    <AppProvider id={appId}>
+      <UserProvider fallback={AnonymousLogin}>
+        <RealmProvider
+          schema={[Task]}
+          sync={{
+            flexible: true,
+            onError: (_session, error) => {
+              console.log(error);
             },
-            rerunOnOpen: true,
-          },
-        }}
-      >
-        {children}
-      </RealmProvider>
-    </UserProvider>
-  </AppProvider>
-);
+            initialSubscriptions: {
+              update(subs, realm) {
+                subs.add(realm.objects("Task"));
+              },
+              rerunOnOpen: true,
+            },
+          }}
+        >
+          {children}
+        </RealmProvider>
+      </UserProvider>
+    </AppProvider>
+  );
+}
